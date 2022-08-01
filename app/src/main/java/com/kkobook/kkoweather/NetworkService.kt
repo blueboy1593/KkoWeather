f52700ddad7355e4e4acd2f9792079a21cf0db0c
Kkobook <snb0303@naver.com>
2022-08-02 02:05:29 +0900

    make retrofit service

--- app/src/main/java/com/kkobook/kkoweather/NetworkService.kt
+++ app/src/main/java/com/kkobook/kkoweather/NetworkService.kt
@@ -3,9 +3,20 @@ package com.kkobook.kkoweather
 
 import com.google.gson.GsonBuilder
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
+
+class RetrofitServiceProvider {
+    fun apiService(): RetrofitService =
+        Retrofit.Builder()
+            .baseUrl("https://newsapi.org")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitService::class.java)
+}
 
 interface NetworkService {
     @GET("v1/search/news.json")
@@ -14,6 +25,10 @@ interface NetworkService {
         @Query("display") display: Int? = null,
         @Query("start") start: Int? = null
     ): Call<WeatherInfo>
+
+    companion object {
+        const val API_KEY = "9781b8438fd44e4aa2be4ac5f419014c"
+    }
 }
 
 //interface NetworkService {
